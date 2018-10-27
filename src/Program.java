f4f51c1952062752185a2184f64e0e329e259837
TuanAnhPham <ptuananh196@gmail.com>
2018-10-27 07:40:44 +0700

    Test github

---
+++ src/Program.java
@@ -0,0 +1,16 @@
+public class Program {
+    public static void  main(String[] args){
+        //hello world
+        String day = "Saturday";
+        int date = 16;
+        String month = "July";
+        int year = 2011;
+        System.out.print("Hello, world. ");
+        System.out.println("How are you?");
+        System.out.println("American format: ");
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+        System.out.println("European format: ");
+        System.out.println(day + " " + date + " " + month + ", " + year);
+
+    }
+}
